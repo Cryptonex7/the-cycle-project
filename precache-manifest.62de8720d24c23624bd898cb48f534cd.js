f5ece69848dcacfe4d9dd93a1b1f9014407ae76c
atharu1 <atharvu1@gmail.com>
2019-03-16 01:26:08 +0530

    Updates

---
+++ precache-manifest.62de8720d24c23624bd898cb48f534cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da0f45e496ee768568a",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/css/main.32d240d7.chunk.css"
+  },
+  {
+    "revision": "1da0f45e496ee768568a",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/main.1da0f45e.chunk.js"
+  },
+  {
+    "revision": "c85ca758bdeee1ef89b5",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/css/1.43adc621.chunk.css"
+  },
+  {
+    "revision": "c85ca758bdeee1ef89b5",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/1.c85ca758.chunk.js"
+  },
+  {
+    "revision": "af4d70916d7df0649273",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/css/2.43adc621.chunk.css"
+  },
+  {
+    "revision": "af4d70916d7df0649273",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/2.af4d7091.chunk.js"
+  },
+  {
+    "revision": "881fbc151eaba7870d6b",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/css/3.6b36f2d8.chunk.css"
+  },
+  {
+    "revision": "881fbc151eaba7870d6b",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/3.881fbc15.chunk.js"
+  },
+  {
+    "revision": "8550de7fd96e5d656d23",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/4.8550de7f.chunk.js"
+  },
+  {
+    "revision": "b98ff755a0f30288c9a8",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/js/runtime~main.b98ff755.js"
+  },
+  {
+    "revision": "a7cd08f0f155735885e5a26429f51ba5",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/media/AC-Logo.a7cd08f0.svg"
+  },
+  {
+    "revision": "6b19c33602d056e921037e70e24b1e50",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/media/facebook.6b19c336.svg"
+  },
+  {
+    "revision": "2f1bca5f8dd1a441662b7ffb26c303e7",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/static/media/googleplus.2f1bca5f.svg"
+  },
+  {
+    "revision": "ad1a12f759d80af4daed5817658a6831",
+    "url": "/Cryptonex7/the-cycle-project/tree/master/client/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Cryptonex7/the-cycle-project/tree/master/client/precache-manifest.62de8720d24c23624bd898cb48f534cd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Cryptonex7/the-cycle-project/tree/master/client/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
